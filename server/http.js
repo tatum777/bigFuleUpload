989d261f2e33435ade6ef44a2811101fc2159c38
wjr <289298820@qq.com>
2020-12-13 14:36:15 +0800

    阶段1: 分片上传合并

---
+++ server/http.js
@@ -0,0 +1,89 @@
+const http = require('http');
+const path = require('path');
+const fse = require('fs-extra');
+const multiparty = require('multiparty');
+
+const server = http.createServer();
+const UPLOAD_DIR = path.resolve(__dirname, '..', 'target');
+
+const resolvePost = req => 
+  new Promise(resolve => {
+    let chunk = '';
+    req.on('data', data => {
+      chunk += data;
+    });
+    req.on('end', () => {
+      resolve(JSON.parse(chunk));
+    })
+  })
+
+const pipeStream = (path, writeStream) =>
+  new Promise(resolve => {
+    const readStream = fse.createReadStream(path);
+    readStream.on('end', () => {
+      fse.unlinkSync(path);
+      resolve();
+    })
+    readStream.pipe(writeStream);
+  })
+
+const mergeFileChunk = async (filePath, filename, size) => {
+  const chunkDir = path.resolve(UPLOAD_DIR, filename);
+  const chunkPaths = await fse.readdir(chunkDir);
+  chunkPaths.sort((a, b) => a.split('-')[1] - b.split('-')[1]);
+  await Promise.all(
+    chunkPaths.map((chunkPath, index) => 
+      pipeStream(
+        path.resolve(chunkDir, chunkPath),
+        fse.createWriteStream(filePath, {
+          start: index * size,
+          end: (index + 1) * size
+        })
+      )
+    )
+  );
+  fse.rmdirSync(chunkDir);
+}
+
+server.on('request', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  if(req.method === 'OPTIONS') {
+    res.status = 200;
+    res.end();
+    return;
+  }
+
+  const multipart = new multiparty.Form();
+
+  multipart.parse(req, async (err, fields, files) => {
+    if(err) {
+      return;
+    }
+    const [chunk] = files.chunk;
+    const [hash] = fields.hash;
+    const [filename] = fields.filename;
+    const chunkDir = path.resolve(UPLOAD_DIR, filename);
+
+    if(!fse.existsSync(chunkDir)) {
+      await fse.mkdirs(chunkDir);
+    }
+    await fse.move(chunk.path, `${chunkDir}/${hash}`);
+    res.end('received file chunk');
+  })
+
+  if(req.url === '/merge') {
+    const data = await resolvePost(req);
+    const { filename, size } = data;
+    const filePath = path.resolve(UPLOAD_DIR, `1${filename}`);    // debugger
+    await mergeFileChunk(filePath, filename, size);
+    res.end(
+      JSON.stringify({
+        code: 0,
+        message: 'file merged success'
+      })
+    )
+  }
+})
+
+server.listen(3000, () => console.log("正在监听 3000 端口"))
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,96 @@
 <template>
   <div id="app">
+    <input type="file" @change="handleFileChange">
+    <el-button @click="handleUpload">上传</el-button>
   </div>
 </template>
 
 <script>
+const SIZE = 10 * 1024 * 1024;
 
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      container: {
+        file: null
+      },
+      data: []
+    }
+  },
+  methods: {
+    request({url, method="post", data, headers={}, requestList}) {
+      return new Promise(resolve => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        Object.keys(headers).forEach(key =>
+          xhr.setRequestHeader(key, headers[key])
+        );
+        xhr.send(data);
+        xhr.onload = e => {
+          resolve({
+            data: e.target.response
+          })
+        }
+      })
+    },
+    handleFileChange(e) {
+      const [file] = e.target.files;
+      if(!file) return;
+      Object.assign(this.$data, this.$options.data());
+      this.container.file = file;
+    },
+    createFileChunk(file, size = SIZE) {
+      const fileChunkList = [];
+      let cur = 0;
+      while(cur < file.size) {
+        fileChunkList.push({file: file.slice(cur, cur + size)});
+        cur += size;
+      }
+      return fileChunkList;
+    },
+    async uploadChunks() {
+      const requestList = this.data
+        .map(({chunk, hash}) => {
+          const formData = new FormData();
+          formData.append('chunk', chunk);
+          formData.append('hash', hash);
+          formData.append('filename', this.container.file.name);
+          return { formData };
+        })
+        .map(async ({formData}) => 
+          this.request({
+            url: 'http://localhost:3000',
+            data: formData
+          })
+        )
+      await Promise.all(requestList);
+      await this.mergeRequest();
+    },
+    async mergeRequest() {
+      await this.request({
+        url: 'http://localhost:3000/merge',
+        headers: {
+          'content-type': 'application/json'
+        },
+        data: JSON.stringify({
+          filename: this.container.file.name,
+          size: SIZE
+        })
+      })
+    },
+    async handleUpload() {
+      if(!this.container.file) return;
+      const fileChunkList = this.createFileChunk(this.container.file);    // [{file: 切片}]
+      this.data = fileChunkList.map(({file}, index) => ({
+        chunk: file,
+        hash: this.container.file.name + '-' + index
+      }));
+      await this.uploadChunks();
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
